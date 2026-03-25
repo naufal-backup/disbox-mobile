e870e930f5195da33e7bf6935ea0f615091e0a41
naufal-backup <alamsyahnaufal453@gmail.com>
2026-03-25 08:47:08 +0700

    fix: bump versionCode to 4 to fix update conflict

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.disbox.mobile"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0.0"
+        versionCode = 4
+        versionName = "3.1.0"
 
         vectorDrawables {
             useSupportLibrary = true
