7ac052191ce27806cf559f8266278759538e067f
naufal-backup <alamsyahnaufal453@gmail.com>
2026-03-26 06:24:37 +0700

    chore: bump version to 3.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.disbox.mobile"
         minSdk = 24
         targetSdk = 35
-        versionCode = 12
-        versionName = "3.5.0"
+        versionCode = 13
+        versionName = "3.5.1"
 
         vectorDrawables {
             useSupportLibrary = true
